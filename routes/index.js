eb16cba68a3c7fc9851d563438ac9e201f31cb8e
John Reynolds <john.reynolds@meredith.com>
2022-03-03 14:34:14 -0500

    Using Elementish to add current time to welcome page.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var currentTime = require('elementish');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'be-fe-ish', currentTime: currentTime});
 });
 
 module.exports = router;
