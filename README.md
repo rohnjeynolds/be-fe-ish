32a5c39086259d8b88c77c62f48ebe1f69387e76
John Reynolds <john.reynolds@meredith.com>
2022-03-10 14:29:20 -0500

    Changing role of Elementish to just provide a welcome message.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # be-fe-ish
+
+Stub application representing a be-fe-* site.
+
+## Install
+
+- `npm install`
+- `npm run start`
+- Point browswer to `http://localhost:3000`.
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var currentTime = require('elementish');
+var welcomeMessage = require('elementish');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'be-fe-ish', currentTime: currentTime});
+  res.render('index', { title: 'be-fe-ish', welcomeMessage: welcomeMessage});
 });
 
 module.exports = router;
